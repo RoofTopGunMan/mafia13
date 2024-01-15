65227b8696ab0e751af8731cc8f189f8160fe38e
dusdnr33 <qewrytu@naver.com>
2024-01-15 19:17:23 +0900

    마피아 인게임 1차
    게임 부분 로비 / 인게임 분리, gradle수정 dbeaver 변경

---
+++ src/main/frontend/src/game/ingame/Ingame.js
@@ -0,0 +1,15 @@
+
+
+import React,  {useEffect, useState} from 'react';
+
+const Ingame = ({ myName}) => {    
+    return (
+        <>
+        <div>
+            인게임 테스트입니다. {myName}
+        </div>
+        </>
+    );
+}
+
+export default Ingame;
