aa800d10ff2d2a864c60bbcc83d354ab184bb77a
dusdnr33 <qewrytu@naver.com>
2024-01-15 17:54:15 +0900

    application db 수정

---
+++ src/main/java/com/lec/spring/repository/Game_roomRepository.java
@@ -0,0 +1,7 @@
+package com.lec.spring.repository;
+
+import com.lec.spring.domain.Game_room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Game_roomRepository extends JpaRepository<Game_room,Long> {
+}
