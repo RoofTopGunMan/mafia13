5d167030b62fd03948e802b44574fda315d0b6a5
dusdnr33 <qewrytu@naver.com>
2024-01-15 14:36:56 +0900

    SpringBoot - React 연동
    https://velog.io/@persestitan/React-%EC%84%9C%EB%B2%84-%EC%8B%9C%EC%9E%91%EA%B3%BC-%EC%A2%85%EB%A3%8C%ED%95%98%EB%8A%94-%EB%B2%95
    vs code에서 터미널로 npm start 한 후 서버 실행하면 확인 가능합니다.

---
+++ src/main/java/com/lec/spring/controller/testController.java
@@ -0,0 +1,12 @@
+package com.lec.spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+    @GetMapping("/api/hello")
+    public String test(){
+        return "TestString";
+    }
+}
