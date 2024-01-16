0cac746fcc72e3ee6430dabb04cc197c7006124a
ohhuigeon <152496918+ohhuigeon@users.noreply.github.com>
2024-01-16 21:42:15 +0900

    User_Authority 이름 수정

--- src/main/java/com/lec/spring/domain/User_authority.java
+++ src/main/java/com/lec/spring/domain/User_authority.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 @Entity
-public class Authority {
+public class User_authority {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;  // PK
