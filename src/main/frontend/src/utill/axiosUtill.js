5d036262bb7cd1694622f0868276e4300df0102a
dusdnr33 <qewrytu@naver.com>
2024-01-16 20:22:42 +0900

    utill.js 주석추가
    사용 범용성 증진을 위한 주석

--- src/main/frontend/src/utill/axiosUtill.js
+++ src/main/frontend/src/utill/axiosUtill.js
@@ -1,5 +1,6 @@
-// 유틸 함수 js입니다.
-
+/* 유틸 함수 js입니다.
+ * 단순하게 사용되는 axios 함수를 추려냈습니다.
+ */
 import axios from "axios";
 
 export function activitation(){
@@ -7,6 +8,7 @@ export function activitation(){
 }
 
 
+// getMapping 시 해당 함수로 호출하면 됩니다.
 export function UtilGetAxios(axiosURL, argJson, responseFunc) {
   axios.get( axiosURL,
   {
@@ -15,6 +17,8 @@ export function UtilGetAxios(axiosURL, argJson, responseFunc) {
   .then(responseFunc)
   .catch(err=>console.log(err));
 }
+
+// PostMapping 시 해당 함수로 호출하면 됩니다.
 export function UtilPostAxios(axiosURL, argJson, responseFunc) {
   axios.post( axiosURL,
   {
