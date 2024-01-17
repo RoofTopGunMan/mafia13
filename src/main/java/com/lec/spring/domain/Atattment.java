189e4c9a8ccecc1f482672d441dfe240be285134
hyenminkim <152479541+hyenminkim@users.noreply.github.com>
2024-01-17 12:49:35 +0900

    도메인 세팅  및 삭제
    Atattment/gameavatar/item/
    Log/Seling/User_records/도메인 세팅
    
    User_jop/User_seling/도메인 삭제

--- src/main/java/com/lec/spring/domain/Atattment.java
+++ src/main/java/com/lec/spring/domain/Atattment.java
@@ -1,4 +1,28 @@
 package com.lec.spring.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class Atattment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id; // PK
+
+    @Column(nullable = false)
+    private  Long user_id;
+
+    @Column(nullable = false)
+    private  String sourcename;
+
+    @Column(nullable = false)
+    private String filename;
+
 }
--- src/main/java/com/lec/spring/domain/Gameavatar.java
+++ src/main/java/com/lec/spring/domain/Gameavatar.java
@@ -1,4 +1,32 @@
 package com.lec.spring.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class Gameavatar {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id; // PK
+
+    @Column(nullable = false)
+    private Long user_id;
+
+    @Column()
+    private int head;
+
+    @Column()
+    private int cloak;
+
+    @Column()
+    private int outline;
+
+
 }
