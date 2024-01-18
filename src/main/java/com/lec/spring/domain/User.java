730d545e58303af32c88267e772d8f4e31a597b7
ohhuigeon <152496918+ohhuigeon@users.noreply.github.com>
2024-01-18 21:46:08 +0900

    로그인 / 회원가입 3차
    기능 구현까지 완료 (추후 수정 필요)

--- src/main/java/com/lec/spring/domain/User.java
+++ src/main/java/com/lec/spring/domain/User.java
@@ -39,6 +39,14 @@ public class User extends BaseEntity{
     @Column(nullable = false)
     private Long gamemoney;
 
+    @PrePersist
+    @PreUpdate
+    private void prePersistPreUpdate() {
+        if (gamemoney == null) {
+            gamemoney = 0L;
+        }
+    }
+
     private Long ingame_status;
 
     // User : Authority / N:M 관계
